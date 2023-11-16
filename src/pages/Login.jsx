d2b75b41b1daa9ab89ab9efa0272bd1346180a28
xFlameLiq <benjamincore2002@hotmail.com>
2023-11-15 19:09:29 -0600

    login action connected to db

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -40,7 +40,7 @@ const Login = () => {
 
       if(data.code === "SUCCESS_AUTH") {
         setMsgAuthSucc(true);
-        location.href = "UserHome";
+        location.href = "/UserHome";
         return
       }
 
