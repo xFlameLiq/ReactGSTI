1eaff231892d5c4e4df7ff45647ffd64a444cf6c
xFlameLiq <benjamincore2002@hotmail.com>
2023-11-16 10:56:59 -0600

    Home css modified

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
+import '../styles/Home.css'
 
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
-      <Link to='/Register'>Register</Link>
+      <Link className="links" to='/Register'>Register</Link>
       <br />
-      <Link to='/Login'>Login</Link>
+      <Link className="links" to='/Login'>Login</Link>
     </div>
   );
 }
